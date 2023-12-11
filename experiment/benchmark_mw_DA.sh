ace3797aa338647b2f286c9484edbb026c899e64
kbkbowo <b09901073@ntu.edu.tw>
2023-12-11 11:35:19 +0800

    add benchmark_DA

---
+++ experiment/benchmark_mw_DA.sh
@@ -0,0 +1,7 @@
+# usage: bash benchmark.sh <GPU_ID> <PATH_TO_BENCHMARK.PY>
+for task in "door-open-v2-goal-observable" "door-close-v2-goal-observable" "basketball-v2-goal-observable" "shelf-place-v2-goal-observable" "button-press-v2-goal-observable" "button-press-topdown-v2-goal-observable" "faucet-close-v2-goal-observable" "faucet-open-v2-goal-observable" "handle-press-v2-goal-observable" "hammer-v2-goal-observable" "assembly-v2-goal-observable"
+do 
+    CUDA_VISIBLE_DEVICES=$1 accelerate launch $2 --env_name $task --n_exps 25 --ckpt_dir "../ckpts/metaworld_DA" --milestone 24 --result_root "results_AVDC_mw_DA"
+done
+
+python org_results_mw.py --results_root "results_AVDC_mw_DA" 
